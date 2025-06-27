a886043b0d3f2f9a8cdf657b17a79653268da0ae
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-06-27 11:02:14 +0200

    Change button wording when maxFiles of upload is 1

--- app/components/verification/FileUpload.tsx
+++ app/components/verification/FileUpload.tsx
@@ -364,7 +364,7 @@ export default function FileUpload({
                 }}
                 className="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cerulean-blue-500"
               >
-                Add more files
+                {requirements.maxFiles === 1 ? "Choose different file" : "Add more files"}
               </button>
               <button
                 type="button"
